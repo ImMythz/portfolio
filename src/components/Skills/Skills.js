a631a95b92683a0bb1c6d65f8119554207977175
Nate Worley <immythz@gmail.com>
2023-08-31 13:53:19 -0500

    added skill tiles and logos

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -7,6 +7,10 @@ import AEMlogo from '../../images/AEM-logo.png'
 import GitLogo from '../../images/Git-logo.svg'
 import AWSLogo from '../../images/AWS_logo.svg'
 import DjangoLogo from '../../images/django-logo.svg'
+import HTMLCSSLogo from '../../images/HTML-CSS-logo.svg'
+import MySQLLogo from '../../images/MySQL-logo.svg'
+import MongoDBLogo from '../../images/MongoDB-logo.svg'
+import TensorFlowLogo from '../../images/TensorFlow-logo.svg'
 import './styles.css'
 
 export default function Skills() {
@@ -18,35 +22,51 @@ export default function Skills() {
             <div className='skills-container'>
                 <div className='skill-tile'>
                     <img src={ReactLogo} alt="python logo" className='skill-img'></img>
-                    <p className='skill-title'>React</p>
+                    <h2 className='skill-title'>React</h2>
                 </div>
                 <div className='skill-tile'>
                     <img src={JavaScriptLogo} alt="python logo" className='skill-img'></img>
-                    <p className='skill-title'>JavaScript</p>
+                    <h2 className='skill-title'>JavaScript</h2>
                 </div>
                 <div className='skill-tile'>
                     <img src={PythonLogo} alt="python logo" className='skill-img'></img>
-                    <p className='skill-title'>Python</p>
+                    <h2 className='skill-title'>Python</h2>
                 </div>
                 <div className='skill-tile'>
                     <img src={TypeScriptLogo} alt="python logo" className='skill-img'></img>
-                    <p className='skill-title'>TypeScript</p>
+                    <h2 className='skill-title'>TypeScript</h2>
                 </div>
                 <div className='skill-tile'>
                     <img src={AEMlogo} alt="python logo" className='skill-img'></img>
-                    <p className='skill-title'>AEM</p>
+                    <h2 className='skill-title'>AEM</h2>
                 </div>
                 <div className='skill-tile'>
                     <img src={GitLogo} alt="python logo" className='skill-img'></img>
-                    <p className='skill-title'>Git</p>
+                    <h2 className='skill-title'>Git</h2>
                 </div>
                 <div className='skill-tile'>
                     <img src={AWSLogo} alt="python logo" className='skill-img'></img>
-                    <p className='skill-title'>AWS</p>
+                    <h2 className='skill-title'>AWS</h2>
                 </div>
                 <div className='skill-tile'>
                     <img src={DjangoLogo} alt="python logo" className='skill-img'></img>
-                    <p className='skill-title'>DJANGO</p>
+                    <h2 className='skill-title'>DJANGO</h2>
+                </div>
+                <div className='skill-tile'>
+                    <img src={HTMLCSSLogo} alt="python logo" className='skill-img'></img>
+                    <h2 className='skill-title'>HTML & CSS</h2>
+                </div>
+                <div className='skill-tile'>
+                    <img src={MySQLLogo} alt="python logo" className='skill-img'></img>
+                    <h2 className='skill-title'>MySQL</h2>
+                </div>
+                <div className='skill-tile'>
+                    <img src={MongoDBLogo} alt="python logo" className='skill-img'></img>
+                    <h2 className='skill-title'>MongoDB</h2>
+                </div>
+                <div className='skill-tile'>
+                    <img src={TensorFlowLogo} alt="python logo" className='skill-img'></img>
+                    <h2 className='skill-title'>TensorFlow</h2>
                 </div>
             </div>
         </div>
