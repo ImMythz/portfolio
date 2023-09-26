e1dc79ce3e97d50f66690456b952012cf494bb6b
Nate Worley <immythz@gmail.com>
2023-09-26 17:31:10 -0500

    fixed tile sizes for skills and portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 export default function About() {
-    let about = 'I grew up in Fort Myers, FL before making the biggest move in my life to Dallas, TX in 2014. It was here in Dallas where I finished High School and began working towards furthering my education in the Software Engineering industry. I currently work as a Front End Developer. My hobbies consist of rock climbing, coding and playing disc golf. Im also deeply interested in AI and Machine Learning and strive to one day have a career in and contribute to the AI/Machine Learning industry.'
+    let about = 'I grew up in Fort Myers, FL before making the biggest move in my life to Dallas, TX in 2014. It was here in Dallas where I finished High School and began working towards furthering my education in the Software Engineering industry. I currently work as a Front End Developer. My hobbies consist of rock climbing, coding and playing disc golf. I am also deeply interested in AI and Machine Learning and strive to one day have a career in and contribute to the AI/Machine Learning industry.'
     return (
         <div className='about-bg'>
             <div className='about-container'>
