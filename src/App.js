f4aa85cc8fc06a2d01fe471497d34aaa16e16a27
Nate Worley <immythz@gmail.com>
2023-09-29 00:30:40 -0500

    deployment test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from './pages/Home';
-
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Home/>
+    <Router basename='/portfolio'>
+      <Routes>
+        <Route exact path='/portfolio' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </Router>
   );
 }
 
